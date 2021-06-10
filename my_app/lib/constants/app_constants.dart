0e941ea7ba7af6f3a6d19af474e9729aefe6dd55
Zishan23 <zishanismam@gmail.com>
2021-06-10 15:21:19 +0600

    BMI commit

---
+++ my_app/lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor('#ffffff');
+final Color accentHexColor = HexColor('#1c58fc');
